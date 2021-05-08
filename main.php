fca6172d3c0a6c9352335ece45af7b1f56e7f50a
elpwc <elpwc@hotmail.com>
2021-05-08 19:35:41 +0800

    小修正

--- main.php
+++ main.php
@@ -1,15 +1,9 @@
 require "dbcfg.php";
 
 
-$q = isset($_GET['q'])? htmlspecialchars($_GET['q']) : '';
+$q = $_POST['q']);
 if($q) {
-        if($q =='RUNOOB') {
-                echo '菜鸟教程<br>http://www.runoob.com';
-        } else if($q =='GOOGLE') {
-                echo 'Google 搜索<br>http://www.google.com';
-        } else if($q =='TAOBAO') {
-                echo '淘宝<br>http://www.taobao.com';
-        }
+        
 }
 
 $link = @mysql_connect(HOST,USER,PASS) or die("提示：数据库连接失败！");
