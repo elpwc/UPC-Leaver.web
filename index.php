b5c88d59bc8787ceff1d587f5eface3eb03d61f2
elpwc/wniko <elpwc@hotmail.com>
2021-11-28 15:01:42 +0800

    新版debug1

--- index.php
+++ index.php
@@ -1,41 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html lang="zh-CN">
 
 <head>
   <meta charset="utf-8" />
-  <title>⊌PC Leaver - 批量申请出校服务</title>
+  <title>⊌PC Leaver.nova - 批量申请出校服务(新版)</title>
   <link rel="stylesheet" href="./lib/main.css" />
   <link rel="icon" href="./src/favicon.ico" sizes="32x32" />
   <!--link rel="stylesheet" href="./lib/bootstrap.min.css" /-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <!--script src="./lib/jquery-3.3.1.min.js"></script-->
   <script src="https://cdn.staticfile.org/jquery/3.3.1/jquery.min.js"></script>
   <script src="./lib/main.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 </head>
 
 <body>
   <div id="body_div" class="container-fluid">
     <div class="head_div">
-      <h1><span id="logo">&nbsp;⊌</span><span id="downp">P</span>C Leaver - 批量申请出校服务&nbsp;<span id="test_title"></span>
+      <h1><span id="logo">&nbsp;⊌</span><span id="downp">P</span>C Leaver.nova - 批量申请出校服务 更新于2021年11月27日&nbsp;<span id="test_title"></span>
       </h1><br>
       <p>
         <?php
-          require "dbcfg.php";
-          $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
-          mysqli_set_charset($link, 'utf8');
-          
-          $sql = "SELECT COUNT(DISTINCT(stu_id)) FROM record;";
-          $result = mysqli_query($link, $sql);
-          if ($result->num_rows > 0) {
-              while ($row = $result->fetch_assoc()) {
-                  echo "已有".$row["COUNT(DISTINCT(stu_id))"]."人使用了此工具";
-              }
+        require "dbcfg.php";
+        $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
+        mysqli_set_charset($link, 'utf8');
+
+        $sql = "SELECT COUNT(DISTINCT(stu_id)) FROM record;";
+        $result = mysqli_query($link, $sql);
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            echo "已有" . $row["COUNT(DISTINCT(stu_id))"] . "人使用了此工具";
           }
-          
+        }
+
         ?>
       </p>
     </div>
@@ -43,27 +40,24 @@
 
       <!--<p class="red">*请确保已连接校园网</p>-->
       <p class="red"><b>由于申请间隔被调整为了1分钟以上，目前只能一天一天地每隔一分钟进行申请，工具后端正在全面改版中orz()</b></p>
-      
-      <input type="text" class="form-control" placeholder="学号 (请正确填入)"
-        name="stuXh"><br>
+
+      <input type="text" class="form-control" placeholder="学号 (请正确填入)" name="stuXh"><br>
       <input type="text" class="form-control" placeholder="姓名 (请正确填入)" name="stuXm"><br>
       <!--
 
       *以上信息可以随意填写
       -->
-      <big>请假天数(1-100)</big><input type="number" class="form-control" id="len_text" value="1" disabled /><br>
+      <b>请假天数(1-100)</b><input type="number" class="form-control" id="len_text" value="1" disabled /><br>
       请假开始日期<input id="start_date" type="date" class="form-control" value="2020-10-10" /><br>
       请假结束日期<br><span id="end_date">2020-10-10</span><br>
 
-      <button id="btn3" class="btn btn-secondary" data-toggle="collapse" data-target="#collapse-div"
-        open="0">点此填写详细信息(必填)</button>
+      <button id="btn3" class="btn btn-secondary" data-toggle="collapse" data-target="#collapse-div" open="0">点此填写详细信息(必填)</button>
       <div class="card">
         <div id="collapse-div" class="collapse">
           <p class="red">这里的信息必须填入才能申请成功，可以按一下下面的随机填入。<br>
             （民族和紧急联系人姓名是按照人数占比随机生成的。其余的都是在均匀分布中随机选择）<br>
             （建议点一下随机填入再自己改内容）</p>
-          <button id="btn4" class="btn btn-dark">随机填入</button>&nbsp;<button id="btn5"
-            class="btn btn-danger">全部清空</button>
+          <button id="btn4" class="btn btn-dark">随机填入</button>&nbsp;<button id="btn5" class="btn btn-danger">全部清空</button>
           <input type="text" class="form-control" placeholder="学院" name="stuXy">
           <input type="text" class="form-control" placeholder="专业" name="stuZy">
           <input type="text" class="form-control" placeholder="民族" name="stuMz">
@@ -78,17 +72,17 @@
           <input type="text" class="form-control" placeholder="外出地址" name="stuWcdz" value="" />
           <input type="text" class="form-control" placeholder="外出紧急联系人" name="stuJjlxr" value="" />
           <input type="text" class="form-control" placeholder="紧急联系人方式" name="stuJjlxrLxfs" value="" />
-          每日外出时间(随意设定即可)<input id="out_time" class="form-control" type="time" value="08:00" />
+          <!--每日外出时间(随意设定即可)<input id="out_time" class="form-control" type="time" value="08:00" />-->
         </div>
       </div>
       <br>
       <div style="background: url('src/bgi.jpg');">
-        <button id="btn1" class="btn btn-primary">一键申请外出</button>
+      <button id="btn1" class="btn btn-primary">一键申请外出</button> <button id="testbtn" class="btn btn-primary">test</button>
         <p class="red" id="online_message" hidden>将于5月上旬上线(大概)</p>
         <span id="tips-message"></span>
         <div id="result-message"></div>
         <div id="result-setsumei-message" class="card">
-          
+
         </div>
         <br>
         *信息仅用于与校方接口验证，不会在服务器存储。<br>
@@ -102,11 +96,10 @@
     <br><br>
 
     <div class="card">
-      <big style="text-align: center;">༺༺匿名留言板༻༻</big><span id="littlescript" style="text-align: center;">(支持除script,
+      <b style="text-align: center;">༺༺匿名留言板༻༻</b><span id="littlescript" style="text-align: center;">(支持除script,
         style外的html标签(小声))</span>
       <input type="text" class="form-control" placeholder="名字 (可留空)" name="comment-name" value="" /><br>
-      <input type="text" class="form-control" placeholder="留言 (有bug和建议的话，也请写在这里orz)" name="comment-text" id="comment-tb"
-        value="" /><br>
+      <input type="text" class="form-control" placeholder="留言 (有bug和建议的话，也请写在这里orz)" name="comment-text" id="comment-tb" value="" /><br>
       <p hidden id="comment-tips" class="red">写上留言才能发送嗷</p>
       <div class="row">
         <div class="col-2">
@@ -117,30 +110,30 @@
 
           <ul class="pagination">
             <?php
-                  $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
-                  mysqli_set_charset($link, 'utf8');
-                  
-                  $sql = "SELECT COUNT(id) FROM messages;";
-                  $result = mysqli_query($link, $sql);
-                  $count = 0;
-                  $each_page = 10;
+            $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
+            mysqli_set_charset($link, 'utf8');
 
-                  if ($result->num_rows > 0) {
-                      while ($row = $result->fetch_assoc()) {
-                          $count = (int)$row["COUNT(id)"];
-                      }
-                  }
-                  $page_num = ceil((double)$count / (double)$each_page);
-                  for ($i = 1; $i<=$page_num; $i++) {
-                      if ($i == 1) {
-                          echo('<li class="page-item"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">第一页</a></li>');
-                      }
-                      echo('<li class="page-item" id="page-no'.(string)$i.'"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">'.(string)$i.'</a></li>');
-                      if ($i == $page_num) {
-                          echo('<li class="page-item"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">最后一页</a></li>');
-                      }
-                  }
-        ?>
+            $sql = "SELECT COUNT(id) FROM messages;";
+            $result = mysqli_query($link, $sql);
+            $count = 0;
+            $each_page = 10;
+
+            if ($result->num_rows > 0) {
+              while ($row = $result->fetch_assoc()) {
+                $count = (int)$row["COUNT(id)"];
+              }
+            }
+            $page_num = ceil((float)$count / (float)$each_page);
+            for ($i = 1; $i <= $page_num; $i++) {
+              if ($i == 1) {
+                echo ('<li class="page-item"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">第一页</a></li>');
+              }
+              echo ('<li class="page-item" id="page-no' . (string)$i . '"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">' . (string)$i . '</a></li>');
+              if ($i == $page_num) {
+                echo ('<li class="page-item"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">最后一页</a></li>');
+              }
+            }
+            ?>
 
           </ul>
         </div>
@@ -153,30 +146,30 @@
 
         <ul class="pagination">
           <?php
-                  $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
-                  mysqli_set_charset($link, 'utf8');
-                  
-                  $sql = "SELECT COUNT(id) FROM messages;";
-                  $result = mysqli_query($link, $sql);
-                  $count = 0;
-                  $each_page = 10;
+          $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
+          mysqli_set_charset($link, 'utf8');
 
-                  if ($result->num_rows > 0) {
-                      while ($row = $result->fetch_assoc()) {
-                          $count = (int)$row["COUNT(id)"];
-                      }
-                  }
-                  $page_num = ceil((double)$count / (double)$each_page);
-                  for ($i = 1; $i<=$page_num; $i++) {
-                      if ($i == 1) {
-                          echo('<li class="page-item"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">第一页</a></li>');
-                      }
-                      echo('<li class="page-item" id="page-no'.(string)$i.'"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">'.(string)$i.'</a></li>');
-                      if ($i == $page_num) {
-                          echo('<li class="page-item"><a class="page-link" href="javascript:comment_reload('.(string)($each_page*($i-1)).','.(string)($each_page*$i-1).','.(string)$i.','.(string)$page_num.');">最后一页</a></li>');
-                      }
-                  }
-        ?>
+          $sql = "SELECT COUNT(id) FROM messages;";
+          $result = mysqli_query($link, $sql);
+          $count = 0;
+          $each_page = 10;
+
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              $count = (int)$row["COUNT(id)"];
+            }
+          }
+          $page_num = ceil((float)$count / (float)$each_page);
+          for ($i = 1; $i <= $page_num; $i++) {
+            if ($i == 1) {
+              echo ('<li class="page-item"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">第一页</a></li>');
+            }
+            echo ('<li class="page-item" id="page-no' . (string)$i . '"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">' . (string)$i . '</a></li>');
+            if ($i == $page_num) {
+              echo ('<li class="page-item"><a class="page-link" href="javascript:comment_reload(' . (string)($each_page * ($i - 1)) . ',' . (string)($each_page * $i - 1) . ',' . (string)$i . ',' . (string)$page_num . ');">最后一页</a></li>');
+            }
+          }
+          ?>
 
         </ul>
       </div>
--- main.php
+++ main.php
@@ -4,6 +4,15 @@ require "dbcfg.php";
 //ob_end_clean();//清空（擦除）缓冲区并关闭输出缓冲
 //ob_implicit_flush(true);
 
+/*
+meta data:
+data: {
+  "app_id":"458","node_id":"","form_data":{"1259":{"User_4":"韦润乾","User_6":"地球科学与技术学院","User_8":"1901040412","User_10":"勘查技术与工程(测井)","User_14":"17865329621","User_55":"壮族","User_60":"勘查(测井)1904","Input_16":"打车","Input_18":"17776231863","Input_22":"购买生活用品","Input_26":"吾悅","Input_28":"李田所","Input_30":"11451419198","Calendar_20":"2021-11-28T16:00:00.000Z","Checkbox_34":[{"value":"1","name":"本人严格按照请假外出要求做好疫情防控工作，一卡通/身份证确保本人使用，所到地点、交通方式、返校时间等均与请假所填写信息一致，在外出期间随时与辅导员保持联系，注重人身安全、财产安全，若发生事故，责任自负。"}]}},"userview":1}
+agent_uid: 
+starter_depart_id: 61408
+test_uid: 0
+*/
+/*
 $data = array(
   'stuXh' => $_POST['stuXh'],  # 学号
   'stuXm' => $_POST['stuXm'],  # 姓名
@@ -20,48 +29,96 @@ $data = array(
   'stuJjlxr' => $_POST['stuJjlxr'],  # 外出紧急联系人
   'stuJjlxrLxfs' => $_POST['stuJjlxrLxfs']  # 紧急联系人联系方式
 );
+*/
+//2021-11-28T16:00:00.000Z
+function getDataByTimeDate($date)
+{
+  return array(
+    'data' => urlencode('{"app_id":"458","node_id":"","form_data":{"1259":{"User_4":"' .
+      $_POST['stuXm'] .
+      '","User_6":"' .
+      $_POST['stuXy'] .
+      '","User_8":"' .
+      $_POST['stuXh'] .
+      '","User_10":"' .
+      $_POST['stuZy'] .
+      '","User_14":"' .
+      $_POST['stuLxfs'] .
+      '","User_55":"' .
+      $_POST['stuMz'] .
+      '","User_60":"' .
+      $_POST['stuBj'] .
+      '","Input_16":"' .
+      $_POST['stuJtfs'] .
+      '","Input_18":"' .
+      $_POST['stuJzdh'] .
+      '","Input_22":"' .
+      $_POST['stuReason'] .
+      '","Input_26":"' .
+      $_POST['stuWcdz'] .
+      '","Input_28":"' .
+      $_POST['stuJjlxr'] .
+      '","Input_30":"' .
+      $_POST['stuJjlxrLxfs'] .
+      '","Calendar_20":"' .
+      $date .
+      '","Checkbox_34":[{"value":"1","name":"本人严格按照请假外出要求做好疫情防控工作，一卡通/身份证确保本人使用，所到地点、交通方式、返校时间等均与请假所填写信息一致，在外出期间随时与辅导员保持联系，注重人身安全、财产安全，若发生事故，责任自负。"}]}},"userview":1}'),
+    'agent_uid' => '',
+    'starter_depart_id' => '61408',
+    'test_uid' => '0'
+  );
+}
+
+
 
 $link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die();
 //mysqli_select_db($link, DBNAME);
 mysqli_set_charset($link, 'utf8');
 
-$sql = "INSERT INTO record (stu_id) VALUES ('".$_POST['stuXh']."');";
+$sql = "INSERT INTO record (stu_id) VALUES ('" . $_POST['stuXh'] . "');";
 $result = mysqli_query($link, $sql);
 
-$date=date_create();
+$date = date_create();
 $startdate = $_POST['startDate'];
 $times = $_POST['times'];
 //$startdate = "2021-05-09";
 //$times = "3";
-date_timestamp_set($date, strtotime($startdate));
+$date = date_timestamp_set($date, strtotime($startdate));
 //$res = array();
 $restext = '|';
 $i = 0;
-while($i < (int)$times){
-
-  $data['stuStartTime'] = date_format($date, "Y-m-d ").$_POST['stuStartTime'].':00';
-  $res = json_decode(send_post("http://stu.gac.upc.edu.cn:8089/stuqj/addQjMess", $data));
-
+while ($i < (int)$times) {
+  //2021-11-28T16:00:00.000Z
+  $timedate = date_format($date, "Y-m-d") . 'T16:00:00.000Z';
+  //$data['stuStartTime'] = date_format($date, "Y-m-d ") . $_POST['stuStartTime'] . ':00';
+  //$res = json_decode(send_post("http://stu.gac.upc.edu.cn:8089/stuqj/addQjMess", $data));
+  $res = curlPost("https://service.upc.edu.cn/site/apps/launch", getDataByTimeDate($timedate));
   //sleep(70);
+  echo getDataByTimeDate($timedate)['data'];
+  echo $timedate;
+  echo 'res|';
+  echo $res;
+  echo  '|end.';
 
-  $msg = isset($res->mess) ?$res->mess: '<span class="red">未知错误，可能学校没有开启接口。</span>';
-  if($msg == "无学号姓名信息")
-  {
+  /*
+  $msg = isset($res->mess) ? $res->mess : '<span class="red">未知错误，可能学校没有开启接口。</span>';
+  if ($msg == "无学号姓名信息") {
     $msg = "<span class='red'>无学号姓名信息</span>";
   }
-  if($msg == "成功")
-  {
+  if ($msg == "成功") {
     $msg = "<span class='green'>成功</span>";
   }
-  $restext.='<p>'.date_format($date, "Y-m-d：").$msg.'</p>';
+  $restext .= '<p>' . date_format($date, "Y-m-d：") . $msg . '</p>';
+  */
   /*
   if($res['resultStat']=='success'){
     echo date_format($date,"Y-m-d：").$res['mess'];
   }else{9
     echo date_format($date,"Y-m-d：").$res['mess'];
   }*/
+  //日期加1天
   date_add($date, date_interval_create_from_date_string("1 day"));
-  
+
   $i++;
 }
 echo $restext;
@@ -70,17 +127,74 @@ echo $restext;
 
 function send_post($url, $post_data)
 {
-    $postdata = http_build_query($post_data);
-    $options = array(
-        'http' => array(
-            'method' => 'POST',
-            //'header' => "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0/r/nAccept:*/*/r/nAccept-Language:zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2/r/nContent-Type:application/x-www-form-urlencoded;charset=UTF-8/r/nX-Requested-With:XMLHttpRequest/r/nOrigin:http://stu.gac.upc.edu.cn:8089/r/nConnection:keep-alive/r/nReferer:http://stu.gac.upc.edu.cn:8089/xswc&appid=200200819124942787&state=2",
-            'header' => "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0/r/nAccept:*/*/r/nAccept-Language:zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2/r/nX-Requested-With:XMLHttpRequest/r/nOrigin:http://stu.gac.upc.edu.cn:8089/r/nConnection:keep-alive/r/nReferer:http://stu.gac.upc.edu.cn:8089/xswc&appid=200200819124942787&state=2",
-            'content' => $postdata,
-            'timeout' => 15 * 60 // 超时时间（单位:s）
-        )
-    );
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    return $result;
+  $postdata = http_build_query($post_data);
+  $options = array(
+    'http' => array(
+      'method' => 'POST',
+      //'header' => "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0/r/nAccept:*/*/r/nAccept-Language:zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2/r/nContent-Type:application/x-www-form-urlencoded;charset=UTF-8/r/nX-Requested-With:XMLHttpRequest/r/nOrigin:http://stu.gac.upc.edu.cn:8089/r/nConnection:keep-alive/r/nReferer:http://stu.gac.upc.edu.cn:8089/xswc&appid=200200819124942787&state=2",
+      'header' =>
+      "Accept:application/json, text/plain, */*/r/n" .
+        "Accept-Encoding: gzip, deflate, br/r/n" .
+        "Accept-Language:zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6/r/n" .
+        "Connection: keep-alive/r/n" .
+        "Content-Length: 1955/r/n" .
+        //"Content-Type: application/x-www-form-urlencoded/r/n" .
+        "Cookie: vjuid=168336; vjvd=cf1a1051c2723709009386cecdefa1a7; vt=153712641/r/n" .
+        "Host: service.upc.edu.cn/r/n" .
+        "Origin: https://service.upc.edu.cn/r/n" .
+        "Referer: https://service.upc.edu.cn/v2/matter/start?id=458/r/n" .
+        "sec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Microsoft Edge\";v=\"96\"/r/n" .
+        "sec-ch-ua-mobile: ?0/r/n" .
+        "sec-ch-ua-platform: \"Windows\"/r/n" .
+        "Sec-Fetch-Dest: empty/r/n" .
+        "Sec-Fetch-Mode: cors/r/n" .
+        "Sec-Fetch-Site: same-origin/r/n" .
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29/r/n" .
+        "X-Requested-With:XMLHttpRequest/r/n",
+      'content' => $postdata,
+      'timeout' => 15 * 60 // 超时时间（单位:s）
+    )
+  );
+  $context = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  return $result;
+}
+
+//post
+function curlPost($url, $post_data = array())
+{
+
+  $header  = array(
+    "Accept" => "application/json, text/plain, */*",
+    "Accept-Encoding" => " gzip, deflate, br",
+    "Accept-Language" => "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "Connection" => " keep-alive",
+    "Content-Length" => " 1955",
+    "Content-Type" => " application/x-www-form-urlencoded",
+    "Cookie" => " vjuid=168336; vjvd=cf1a1051c2723709009386cecdefa1a7; vt=153712641",
+    "Host" => " service.upc.edu.cn",
+    "Origin" => " https://service.upc.edu.cn",
+    "Referer" => " https://service.upc.edu.cn/v2/matter/start?id=458",
+    "sec-ch-ua" => " \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Microsoft Edge\";v=\"96\"",
+    "sec-ch-ua-mobile" => " ?0",
+    "sec-ch-ua-platform" => " \"Windows\"",
+    "Sec-Fetch-Dest" => " empty",
+    "Sec-Fetch-Mode" => " cors",
+    "Sec-Fetch-Site" => " same-origin",
+    "User-Agent" => " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29",
+    "X-Requested-With" => "XMLHttpRequest",
+  );
+
+  if (is_array($post_data)) {
+    $post_data = http_build_query($post_data, '', '&');
+  }
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+  $output = curl_exec($ch);
+  curl_close($ch);
+  return $output;
 }
